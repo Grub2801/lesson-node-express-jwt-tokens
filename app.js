5d4189405f4f62bf44a4f767861ca90145116922
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-18 12:50:03 +0800

    in lab codealong

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ bodyParser  = require('body-parser'),
 mongoose    = require('mongoose'),
 app         = express(),
 logger      = require('morgan');
+expressJWT  = require('express-jwt'),
+jwt         = require('jsonwebtoken'),
+Agent = require('./models/agent');
 
+var secret = "onhermajestyssecretservice";
 mongoose.connect('mongodb://localhost:27017/MI6');
 
 app.use(logger('dev'));
@@ -19,6 +23,34 @@ if (app.get('env') === 'development') {
     });
   });
 }
+
+app.use('/api/agents/:id', expressJWT({secret: secret}));
+app.use(function (error, request, response, next) {
+  // send an appropriate status code & JSON object saying there was an error, if there was one.
+  console.log(error);
+  if (error.name === 'UnauthorizedError') {
+    response.status(401).json({message: 'You need an authorization token to view confidential information.'});
+  }
+});
+
+
+app.post('/api/authorizations', function(request, response){
+  var agentParams = request.body;
+  // some code to check that a user's credentials are right #bcryptmaybe?
+  Agent.findOne({codename: agentParams.codename}, function (err, agent) {
+    if (!agent) return response.status(404).json({message: "There is no agent by that codename"});
+    agent.authenticate(agentParams.name, function (err, isMatch) {
+      if (isMatch) {
+        // collect any information we want to include in the token, like that user's info
+        // make a token already & send it as JSON
+        var token = jwt.sign(agent, secret);
+        return response.json({agent: agent, token: token});
+      } else {
+        return response.status(401).json({message: "You dont have clearence, agent"});
+      }
+    });
+  });
+});
 
 var routes = require('./config/routes');
 app.use('/api', routes);
